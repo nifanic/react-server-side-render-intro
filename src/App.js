4ca2d7722e40284bc530983212edf9361beacda1
nifanic <nifanic@masterpage.net>
2020-03-08 00:21:29 -0500

    Create `Home` component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,4 @@
-import React from 'react';
+import React from "react";
+import Home from "./Home";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+export default () => <Home name="Alligator" />;
